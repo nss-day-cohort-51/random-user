5695dd16a6f2a9fc142da0d56d576c35bb459157
householdgenius <emily.reed0913@gmail.com>
2021-09-07 10:41:19 -0500

    created a function to display a random user in the console

---
+++ scripts/user.js
@@ -0,0 +1,8 @@
+export const getUsers = () => {
+    return fetch("https://randomuser.me/api/?exc=login")
+        .then((response) => {
+            return response.json()
+
+        })
+        .then(parsedResponse => console.log(parsedResponse))
+}
