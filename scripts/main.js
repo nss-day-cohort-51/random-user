f1074c23d03d23f334845e2ec704e1cfae025b0c
Jake Lashenik <jmlashen@gmail.com>
2021-09-07 16:18:29 -0500

    added event listener to button to change user

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,22 @@ import {getRandomUser} from "./data.js"
 import { User } from "./user.js";
 import { UserList } from "./userlist.js";
 
-// getRandomUser()
-// .then(response => console.log(response)) 
-// console.log("You look marvelous!")
 
 const showUserList = () => {
     const userElement = document.querySelector(".userlist");
     getRandomUser().then(userPosts => {
         userElement.innerHTML = UserList(userPosts.results);
     })
-}
 
+
+}
+const applicationElement = document.querySelector("#newUser");
+
+applicationElement.addEventListener("click", event => {
+    showUserList()
+   
+})
+
+// const startnewUser = () => {
 showUserList()
+// }
