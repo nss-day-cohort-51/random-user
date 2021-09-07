fdc67d4fa8f8f103c651d60e28d2f7b073101464
joshuastewart417 <joshua.stewart780@gmail.com>
2021-09-07 14:58:27 -0500

    added data card with return HTML to show user data

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 import { fetchRandomUser } from "./data/datamanager.js";
+import { userHTML } from "./data/data.js";
 
-const info = fetchRandomUser().then((apiUser) => {
-  console.log(apiUser);
-});
+const showUser = () => {
+  //Get a reference to the location on the DOM where the nav will display
+  const entryElement = document.querySelector(".userInfo");
+  fetchRandomUser().then((apiUser) => {
+    entryElement.innerHTML = userHTML(apiUser);
+  });
+};
+
+showUser();
