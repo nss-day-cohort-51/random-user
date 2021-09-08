9153304b00a7fbd4803ec7362378c6e387c4d03a
GersonGmd <gersondiketama@yahoo.com>
2021-09-08 09:29:31 -0500

    fixed CSS3

--- scripts/userList.js
+++ scripts/userList.js
@@ -1,14 +1,17 @@
+
 export const info = (infos) =>
 {
     return `
-    <img src=${infos.results[0].picture.large}>
-    <li>Name: ${infos.results[0].name.title}. ${infos.results[0].name.first} ${infos.results[0].name.last}</li>
-    <p>Country: ${infos.results[0].location.country}</p>
-    <p>DOB: ${(infos.results[0].dob.date).split("T")[0]}</p>
-    <p>Age: ${infos.results[0].dob.age}</p>
-    <p>Phone: ${infos.results[0].cell}</p>
-    <p>Email: ${infos.results[0].email}</p>
+    <div class="infosDisplay">
+    <img class="images" src=${infos.results[0].picture.large}>
+    <li>Name: <span class="color">${infos.results[0].name.title}. ${infos.results[0].name.first} ${infos.results[0].name.last}</span></li>
+    <li>Country: <span class="color">${infos.results[0].location.country}</span></li>
+    <li>DOB: <span class="color">${new Date(infos.results[0].dob.date).toLocaleString()}</span></li>
+    <li>Age: <span class="color">${infos.results[0].dob.age}</span></li>
+    <li>Phone: <span class="color">${infos.results[0].cell}</span></li>
+    <li>Email:<span class="color"> ${infos.results[0].email}</span></li>
     
+    </div>
     
     `
 }
